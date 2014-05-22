1b7e1c8b1e141da8b20908334f855d187eeca9d5
Chugga <chuggga@gmail.com>
2014-05-22 11:39:38 +1000

    Update Server.php
    Added the ability to set whether assets are returned securely or not by adding an 'ssl_enabled' flag to the app config file.

--- src/Basset/Server.php
+++ src/Basset/Server.php
@@ -280,7 +280,11 @@ class Server {
      */
     public function buildAssetUrl($path)
     {
-        return starts_with($path, '//') ? $path : $this->app['url']->asset($path);
+         if(isset($this->app['config']->get('app')['ssl_enabled']) && $this->app['config']->get('app')['ssl_enabled']) {
+            return starts_with($path, '//') ? $path : $this->app['url']->secureAsset($path);
+        } else {
+            return starts_with($path, '//') ? $path : $this->app['url']->asset($path);
+        }
     }
 
 }
